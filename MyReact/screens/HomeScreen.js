d5553dddfc1103c0d8c522d4935eaa68f90a3f1e
Muhammed Thasneem P Android <muhammed.thasneem@yahoo.com>
2018-03-18 08:59:30 +0530

    home screen color changed

--- MyReact/screens/HomeScreen.js
+++ MyReact/screens/HomeScreen.js
@@ -101,7 +101,7 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff000',
   },
   developmentModeText: {
     marginBottom: 20,
